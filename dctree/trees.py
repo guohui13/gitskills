a55a65361b52e6b5c94c5c708ad6883a097a2014
guohui13 <guohui13@163.com>
2019-07-16 13:23:56 +0800

    test git 20180214 comments

---
+++ dctree/trees.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     trees
+   Description :
+   Author :       gh
+   date：         2019-07-16    
+-------------------------------------------------
+   Change Activity:
+                   19-7-16:
+-------------------------------------------------
+"""
+__author__ = 'gh'
+
+
+from numpy import *
+import operator
+
+
+def createdataset()-> list:
+    dataset = [[1, 1, 'yes'], [1, 1, 'yes'], [1, 0, 'no'], [0, 1, 'no'], [0, 1, 'no']]
+    labels = ['no surfacing', 'flippers']
+    return dataset,labels
+
+
+
+if __name__ == '__main__':
+    daset,labels = createdataset()
+    print(daset)
+    print(labels)
