6345a4d6a083c0172c94ab06a0eefcdb15dab19b
guohui13 <guohui13@163.com>
2019-08-07 18:04:58 +0800

    test git 20180214 comments

--- dctree/trees.py
+++ dctree/trees.py
@@ -18,17 +18,40 @@ import operator
 
 
 def createdataset()-> list:
-    dataset = [[1, 1, 'yes'], [1, 1, 'yes'], [1, 0, 'no'], [0, 1, 'no'], [0, 1, 'no']]
+    dataset = [[1, 1, 'yes'],
+               [1, 1, 'yes'],
+               [1, 0, 'no'],
+               [0, 1, 'no'],
+               [0, 1, 'no']]
     labels = ['no surfacing', 'flippers']
     return dataset,labels
 
-def calcShannonEnt(dataset:ndarray) -> float :
-    numEntries = len(dataset)
-    labelCounts = {}
+def calcShannonEnt(dataset) -> float :  # 计算熵的函数，根据频率概率和log函数计算
+    numEntries = len(dataset) # 总长度做分母
+    labelCounts = {} # 利用字典统计各个类别出现的次数
     for featVec in dataset:
-        currentLabel = featVec[-1]
+        currentLabel = featVec[-1] #类别分类值
+        '''if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1'''
+        labelCounts[currentLabel] = labelCounts.setdefault(currentLabel, 0) + 1 # 类别计数器
+        shannonEnt = 0.0 # 初始化熵为0
+    for key in labelCounts: #
+        prob = float(labelCounts[key])/numEntries
+        print(key,'->',labelCounts[key],':',prob)
+        shannonEnt -= prob*math.log(prob, 2)
+
+    return shannonEnt
 
 
+def splitDataSet(dataset:ndarray,axis:int,value:float) -> ndarray:
+    retDataset = []
+    for featVec in dataset:
+        if featVec[axis] == value:
+            reduceFeatVec = featVec[:axis]
+            reduceFeatVec.extend(featVec[axis + 1:])
+            retDataset.append(reduceFeatVec)
+    return retDataset
 
 
 
@@ -36,3 +59,5 @@ if __name__ == '__main__':
     daset,labels = createdataset()
     print(daset)
     print(labels)
+    shan=calcShannonEnt(daset)
+    print(shan)
